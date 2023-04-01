3e0b307edc5e2f7f6b61d24c1a41cad19283630e
marquin.e <marcos.emanuel.04@gmail.com>
2023-04-01 18:38:51 -0300

    :recycle: Fix profile.jpeg to profile.jpg, added CSS for utilities, created the correct home page and added the _app from React

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,10 @@
 import Link from 'next/link'
 import Head from 'next/head';
+import Layout from '../../components/layout';
 
 export default function FirstPost(){
     return (
-        <>
+        <Layout>
         <Head>
             <title>First Post</title>
         </Head>
@@ -12,7 +13,7 @@ export default function FirstPost(){
             <h2>
                 <Link href="/">Back to home</Link>
             </h2>
-        </>
+        </Layout>
 
     )
 }
